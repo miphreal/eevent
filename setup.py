819d060943f84a57956a8861025d210504c158fa
miphreal <miphreal@gmail.com>
2013-01-31 00:30:56 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='eevent',
+    version='0.1',
+    packages=['eevent'],
+    url='',
+    license='MIT',
+    author='Miphreal Adler',
+    author_email='miphreal@gmail.com',
+    description='Simple lib to organize synchronous hierarchical events'
+)
